acbbc52f7564b5d7e853339dbb9ec8c7c485c862
Robbie Harwood <rharwood@club.cc.cmu.edu>
2018-01-21 11:36:40 -0500

    Switch to JSON for storage backend

--- tood.py
+++ tood.py
@@ -1,62 +1,43 @@
 #!/usr/bin/env python2
 
 import curses
+import json
 import os
 import sys
 
 from git import Git
 
-def store(git, filepath, done, queue, msg="Update"):
+def store(git, filepath, state, msg="Update"):
     # TODO visual feedback on store
-    data = ""
 
-    for d in done:
-        data += "- [X] %s\n" % d
-        pass
-
-    for q in queue:
-        data += "- [ ] %s\n" % q
-        pass
-
-    open(filepath, "w").write(data)
+    json.dump(state, open(filepath, "w"))
 
     git.add(filepath)
     git.commit("-m", msg)
     pass
 
 def load(filepath):
-    # TODO this can't deal with line wrapping
-    data = open(filepath, "r").read().split("\n")
-    queue = []
-    done = []
-    for line in data:
-        if len(line) <= 0:
-            continue
-        elif line.startswith("- [X] "):
-            done.append(line[len("- [X] "):])
-            continue
-        elif line.startswith("- [ ] "):
-            queue.append(line[len("- [ ] "):])
-            continue
-        print("Parse error on line: %s\n" % line)
-        pass
-    return (queue, done)
+    return json.load(open(filepath, "r"))
 
-def display_queue(stdscr, queue, rows):
+def inc_ind(i):
+    if i == 9:
+        return 0
+    elif i != ' ':
+        return i + 1
+    return i        
+
+def display_state(stdscr, state, rows):
     stdscr.erase()
     stdscr.move(0, 0)
     i = 1
-    for t in queue[:rows]:
-        stdscr.addstr("%s: [ ] %s\n" % (str(i), t))
-        if i == 0:
-            i = ' '
-            pass
-        elif i == 9:
-            i = 0
-            pass
-        elif i != ' ':
-            i += 1
-            pass
+    # TODO handle more than can fit on the screen
+    for t in state["queue"]:
+        stdscr.addstr("%s: [ ] %s\n" % (str(i), t["text"]))
+        i = inc_ind(i)
+        pass
+    for t in state["done"]:
+        stdscr.addstr("%s: [X] %s\n" % (str(i), t["text"]))
+        i = inc_ind(i)
         pass
     return
 
@@ -73,7 +54,7 @@ def get_index(stdscr):
 
 def main(stdscr):
     storagedir = "/home/frozencemetery/.toodata"
-    filename = "tood.org"
+    filename = "tood.json"
 
     if not os.path.exists(storagedir):
         os.mkdir(storagedir)
@@ -89,16 +70,16 @@ def main(stdscr):
 
     filepath = os.path.join(storagedir, filename)
     if not os.path.exists(filepath):
-        open(filepath, "w").write("- [ ] add some TOOD\n")
-        git.add(filepath)
-        git.commit("-m", "Create new TOOD")
+        state = {"queue": [{"text": "Make some TOOD"}],
+                 "done": [{"text": "Create list"}]}
+        store(git, filepath, state, "Create list")
         pass
 
-    queue, done = load(filepath)
+    state = load(filepath)
 
     # TODO handle stupid wide lines frig
     rows, cols = stdscr.getmaxyx()
-    display_queue(stdscr, queue, rows)
+    display_state(stdscr, state, rows)
     prompt(stdscr, rows)
 
     while True:
@@ -107,24 +88,25 @@ def main(stdscr):
         if c == ord('q'):
             break
         elif c == ord('w'):
-            store(git, filepath, done, queue)
+            store(git, filepath, state)
             pass
         elif c == ord('c'):
             stdscr.addstr("c ")
             curses.echo()
             desc = stdscr.getstr()
             curses.noecho()
-            queue.append(desc)
-            display_queue(stdscr, queue, rows)
+            state["queue"].append({"text": desc})
+            display_state(stdscr, state, rows)
             prompt(stdscr, rows)
             pass
         elif c == ord('t'):
             # TODO display as done without removing until next write?
+            # TODO toggling of done things
             stdscr.addstr("t ")
             n = get_index(stdscr)
-            t = queue.pop(n)
-            done.append(t)
-            display_queue(stdscr, queue, rows)
+            t = state["queue"].pop(n)
+            state["done"].insert(0, t)
+            display_state(stdscr, state, rows)
             prompt(stdscr, rows)
             pass
         elif c == ord('m'):
@@ -135,11 +117,11 @@ def main(stdscr):
 
             tgt_ind = get_index(stdscr)
 
-            t = queue.pop(cur_ind)
+            t = state["queue"].pop(cur_ind)
 
-            queue.insert(tgt_ind, t)
+            state["queue"].insert(tgt_ind, t)
 
-            display_queue(stdscr, queue, rows)
+            display_state(stdscr, state, rows)
             prompt(stdscr, rows)
             pass
         pass
