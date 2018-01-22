94c8f07ac76858fd0d271641f661c27663d8c83c
Robbie Harwood <rharwood@redhat.com>
2018-01-22 15:53:11 -0500

    Add python3 re-exec guard
    Without this, just enough of the program runs under python3 to create
    a database that python2 can't open, and then subsequently crash.

--- tood.py
+++ tood.py
@@ -138,5 +138,12 @@ def main(stdscr):
     return
 
 if __name__ == "__main__":
+    # some distros make python be python3, which is... brave
+    if sys.version_info.major != 2:
+        print("I'm python2-only; trying to switch interpreters...")
+        # argv[0] needs to be the interpreter; python hides this, making
+        # argv[0] the program being interpreted
+        exit(os.execlp("python2", "python2", sys.argv[0]))
+
     curses.wrapper(main)
     pass
